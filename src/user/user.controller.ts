65973b8acfc222126742108343cb1b380b08e73f
emanuelplanko <planko.emanuel@gmail.com>
2022-08-21 20:38:53 +0200

    verzija 1-user modul, controller, service

--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -1,13 +1,22 @@
 import {Controller, Get} from '@nestjs/common';
+import {UserService} from "./user.service";
 
 //prefix pri controllerju ti pove na kateri endpoint bo tolkel
 @Controller('users')
 export class UserController {
+    constructor(
+        //UserService je podatkovni tip
+        //da lahko prideš iz controllerja user v service je bilo treba v konstruktor jnastavit, da je spremenljivka userService tako kot vektor-pointer nad userService modul
+        private userService: UserService) {
+    }
 
     //prva funkcija-all
-    @Get()
+    //to kar piše v @Get je endpoint
+    //osnova je users, vsi je nov endpoint
+    @Get('vsi')
     all() {
-        return 'VSS je zakon!';
+        //poiščeš funkcijo all, ki si jo prej naredil v user.service.ts
+        return this.userService.all();
     }
 
 
--- src/user/user.service.ts
+++ src/user/user.service.ts
@@ -1,4 +1,29 @@
 import { Injectable } from '@nestjs/common';
+import {InjectRepository} from "@nestjs/typeorm";
+import {User} from "./user.entity";
+import {Repository} from "typeorm";
 
 @Injectable()
-export class UserService {}
+export class UserService {
+    //se bo avtomatsko pognal ko ga obmo rabli
+    constructor(
+        //userRepository je ime spremenljivke oz. ime tega objekta
+        //podatkovni tip Repository<User>-od userja
+        //tale konstruktor je narejen da v spremenljivko userRepository naredi dostop do baze
+        @InjectRepository(User) private readonly userRepository: Repository<User>
+
+    ) {
+    }
+
+    //funkcija all vrača userje v obliki arraya
+    //funkcija lahko vrača več kot enga userja
+    //return this se osredotoči na spremenljivko userRepository
+    //ker dam brez pogoja mi bo find vrnil vse kar je v tabeli user
+    //da lahko iz userjev dostopam do servica, moram imeti v konstruktorju kazalec-pointer-vektor do sem in isto mora imeti service kazalec-pointer na repository
+    //repository je spremenljivka userRepository in on se poveže na tabelo user
+    //modul je kot api ki skrbi za vse-starš, znotraj tega imamo kontroler ki skrbi samo za en pointer
+   async all(): Promise<User[]> {
+        return this.userRepository.find();
+    }
+
+}
